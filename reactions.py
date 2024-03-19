11ef0d3933bdbd89d16844a87ee02761a52ae445
Bomberaid <alvin1312@icloud.com>
2024-03-19 01:17:07 -0600

    Added general pseudo code to reactions.py

--- reactions.py
+++ reactions.py
@@ -1,5 +1,30 @@
 from ili9341 import Display, color565
+from hcsr04 import HCSR04
 
-class Reaction:
-    def idle(self, lcd_display: Display, ultrasonic_sensor):
-        lcd_display.draw_image()
+class Reactions:
+    def __init__(self, lcd_display: Display, ultrasonic_sensor: HCSR04) -> None:
+        self.lcd_display = lcd_display
+        self.ultrasonic_sensor = ultrasonic_sensor
+        self.movement_speed_factor = 1
+
+    def idle(self, face_image: str):
+        self.lcd_display.draw_image(face_image)
+        # move calmly
+
+    def greeting(self, face_image: str):
+        self.lcd_display.draw_image(face_image)
+
+    def response(self, face_image: str):
+        self.lcd_display.draw_image(face_image)
+
+    def annoyed(self, face_image: str):
+        self.lcd_display.draw_image(face_image)
+
+class Warm_Up_Reactions(Reactions):
+    def __init__(self, lcd_display: Display, ultrasonic_sensor: HCSR04) -> None:
+        super().__init__(lcd_display, ultrasonic_sensor)
+        self.movement_speed_factor = 1.2
+
+    def idle(self, face_image:str):
+        self.lcd_display.draw_image(face_image)
+        # move energeticly
